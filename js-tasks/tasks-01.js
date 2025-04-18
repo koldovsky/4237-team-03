16b7b0aa9a0715c3ea557f8a6d037af18c7c5fed
Max Skydanchuk <skydanchukm@gmail.com>
2025-04-18 14:00:09 +0000

    Updated js - task 6

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -36,13 +36,6 @@ function litres(time) {
 // Task 6
 // Opposites Attract 
 // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
-function lovefunc(flower1, flower2){
-    if ((flower1 % 2 === 0) + (flower2 % 2 === 0) === 1) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
 function lovefunc(flower1, flower2){
   return (flower1 % 2 === 0) != (flower2 % 2 === 0);
