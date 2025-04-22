f1ac4dd8ef72796367ea8db8b89dc387d5cd066e
Max Skydanchuk <skydanchukm@gmail.com>
2025-04-22 21:56:05 +0000

    Added filter modal window

--- js/init.js
+++ js/init.js
@@ -1,12 +1,11 @@
+function init() {
+  import('./main.js');
+}
+
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
 let loadedPartialsCount = 0;
 
-document.body.addEventListener('htmx:afterOnLoad', async () => {
+document.body.addEventListener('htmx:afterOnLoad', () => {
   loadedPartialsCount++;
-  if (loadedPartialsCount === totalPartials) {
-    const module = await import('./main.js');
-    if (module.runApp) {
-      module.runApp(); 
-    }
-  }
+  if (loadedPartialsCount === totalPartials) init();
 });
--- js/main.js
+++ js/main.js
@@ -1,129 +1,352 @@
-class Book {
-    constructor(image, name, price, genre) {
-        this.image = image;
-        this.name = name;
-        this.price = price;
-        this.genre = genre;
-    }
-}
-
 //Max Skydanchuk
-export function runApp() {
-
-    //Creating list with all books, and genre for later sorting
-    const list = [];
-    
-    list.push(new Book("img/books/michael-kane--ascendant.png", "Ascendant - Michael Kane", 25.00, "Lifestyle"));
-    list.push(new Book("img/books/lily-monroe--broken-wings.png", "Broken Wings - Lily Monroe", 22.00, "Lifestyle"));
-    list.push(new Book("img/books/chloe-woods--deadly-secrets.png", "Deadly Secrets - Chloe Woods", 22.00, "Lifestyle"));
-    list.push(new Book("img/books/jasper-lane--digital-ghosts.png", "Digital Ghosts - Jasper Lane", 26.45, "Lifestyle"));
-    list.push(new Book("img/books/lily-monroe--fallen-stars.png", "Fallen Stars - Lily Monroe", 16.50, "Lifestyle"));
-    list.push(new Book("img/books/chloe-woods--fatal-promise.png", "Fatal Promise - Chloe Woods", 21.00, "Lifestyle"));
-    list.push(new Book("img/books/chloe-woods--hidden-truth.png", "Hidden Truth - Chloe Woods", 23.00, "Lifestyle"));
-    list.push(new Book("img/books/jasper-lane--hollow-veil.png", "Hollow Veil - Jasper Lane", 32.50, "Lifestyle"));
-    list.push(new Book("img/books/iris-west--melody.png", "Melody - Iris West", 35.10, "Lifestyle"));
-    list.push(new Book("img/books/lily-monroe--midnight-shadows.png", "Midnight Shadows - Lily Monroe", 20.90, "Lifestyle"));
-    list.push(new Book("img/books/nathan-cole--mirage.png", "Mirage - Nathan Cole", 21.00, "Lifestyle"));
-    list.push(new Book("img/books/liam-hayes--phantom.png", "Phantom - Liam Hayes", 22.45, "Lifestyle"));
-    list.push(new Book("img/books/damon-cross--rebirth.png", "Rebirth - Damon Cross", 23.50, "Lifestyle"));
-    list.push(new Book("img/books/emilia-fox--resonance.png", "Resonance - Emilia Fox", 19.50, "Lifestyle"));
-    list.push(new Book("img/books/oliver-reid--shadow-queen.png", "Shadow Queen - Oliver Reid", 21.99, "Lifestyle"));
-    list.push(new Book("img/books/noah-black--silent-waves.png", "Silent Waves - Noah Black", 23.55, "Lifestyle"));
-    list.push(new Book("img/books/naomi-woods--specter.png", "Specter - Naomi Woods", 29.00, "Lifestyle"));
-    list.push(new Book("img/books/sophie-lark--starlight.png", "Starlight - Sophie Lark", 32.55, "Lifestyle"));
-    list.push(new Book("img/books/lara-miles--stars-of-andromeda.png", "Stars of Andromeda - Lara Miles", 29.99, "Lifestyle"));
-    list.push(new Book("img/books/chloe-woods--stolen-hearts.png", "Stolen Hearts - Chloe Woods", 21.00, "Lifestyle"));
-    list.push(new Book("img/books/alex-draven--the-quantum-prison.png", "The Quantum Prison - Alex Draven", 17.00, "Lifestyle"));
-    list.push(new Book("img/books/lila-gray--the-shadow.png", "The Shadow - Lila Gray", 13.00, "Lifestyle"));
-    list.push(new Book("img/books/amelia-pierce--the-thief-of-time.png", "The Thief of Time - Amelia Pierce", 18.00, "Lifestyle"));
-    list.push(new Book("img/books/oliver-reid--the-wild-hunt.png", "The Wild Hunt - Oliver Reid", 10.00, "Lifestyle"));
-    list.push(new Book("img/books/edgar-hale--thorn-of-the-frost-king.png", "Thorn of The Frost King - Edgar Hale", 29.45, "Lifestyle"));
-    list.push(new Book("img/books/marina-holt--tides.png", "Tides - Marina Holt", 25.22, "Lifestyle"));
-    list.push(new Book("img/books/rebecca-cole--unseen-eyes.png", "Unseen Eyes - Rebecca Cole", 27.00, "Lifestyle"));
-    list.push(new Book("img/books/lily-monroe--whispered-secrets.png", "Whispered Secrets - Lily Monroe", 20.00, "Lifestyle"));
-    list.push(new Book("img/books/lily-monroe--shattered-dreams.png", "Shattered Dreams - Lily Monroe", 19.99, "Lifestyle"));
-
-    const parent = document.getElementById("all-books_books");
-    const loadMoreBtn = document.getElementById("all-books__load-more");
-    const BOOKS_PER_PAGE = 6;
-    let currentPage = 0;
-
-    function renderBooks() {
-        const start = currentPage * BOOKS_PER_PAGE;
-        const end = start + BOOKS_PER_PAGE;
-        const currentBooks = list.slice(start, end);
-
-        currentBooks.forEach(book => {
-            const bookDiv = document.createElement("div");
-            bookDiv.classList.add("all-books__book");
-
-            const img = document.createElement("img");
-            img.src = book.image;
-            img.alt = book.name;
-
-            const name = document.createElement("h4");
-            name.textContent = book.name;
-
-            const price = document.createElement("p");
-            price.textContent = `$${book.price.toFixed(2)} USD`;
-
-            const button = document.createElement("a");
-            button.href = "#";
-            button.textContent = "Buy Now";
-
-            bookDiv.appendChild(img);
-            bookDiv.appendChild(name);
-            bookDiv.appendChild(price);
-            bookDiv.appendChild(button);
-
-            parent.appendChild(bookDiv);
-        });
-
-        currentPage++;
-
-        // Ховаємо кнопку, якщо всі книги вже показані
-        if (currentPage * BOOKS_PER_PAGE >= list.length) {
-            loadMoreBtn.style.display = "none";
-        }
-    }
-
-    // Показати перші 6 книг
-    renderBooks();
-
-    // Обробка натискання на "Load More"
-    loadMoreBtn.addEventListener("click", () => {
-        renderBooks();
-    });
-
-
-    //noUiSlider
-    const slider = document.getElementById("all-books__price-slider");
-    const inputMin = document.querySelector(".all-books__price-input--min");
-    const inputMax = document.querySelector(".all-books__price-input--max");
-
-    noUiSlider.create(slider, {
-        start: [13, 32.5],
-        connect: true,
-        step: 0.5,
-        range: {
-          min: 13,
-          max: 32.5,
-        },
-    });
-
-    slider.noUiSlider.on("update", function (values, handle) {
-        const [min, max] = values;
-        inputMin.value = min;
-        inputMax.value = max;
-    });
-
-    inputMin.addEventListener("change", function () {
-        slider.noUiSlider.set([this.value, null]);
-    });
-
-    inputMax.addEventListener("change", function () {
-        slider.noUiSlider.set([null, this.value]);
-    });
+class Book {
+  constructor(image, name, price, genre) {
+    this.image = image;
+    this.name = name;
+    this.price = price;
+    this.genre = genre;
+  }
 }
-//End Max
+
+//Creating list with all books, and genre for later sorting
+const list = [];
+
+list.push(
+  new Book(
+    "img/books/michael-kane--ascendant.png",
+    "Ascendant - Michael Kane",
+    25.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/lily-monroe--broken-wings.png",
+    "Broken Wings - Lily Monroe",
+    22.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/chloe-woods--deadly-secrets.png",
+    "Deadly Secrets - Chloe Woods",
+    22.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/jasper-lane--digital-ghosts.png",
+    "Digital Ghosts - Jasper Lane",
+    26.45,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/lily-monroe--fallen-stars.png",
+    "Fallen Stars - Lily Monroe",
+    16.5,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/chloe-woods--fatal-promise.png",
+    "Fatal Promise - Chloe Woods",
+    21.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/chloe-woods--hidden-truth.png",
+    "Hidden Truth - Chloe Woods",
+    23.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/jasper-lane--hollow-veil.png",
+    "Hollow Veil - Jasper Lane",
+    32.5,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/iris-west--melody.png",
+    "Melody - Iris West",
+    35.1,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/lily-monroe--midnight-shadows.png",
+    "Midnight Shadows - Lily Monroe",
+    20.9,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/nathan-cole--mirage.png",
+    "Mirage - Nathan Cole",
+    21.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/liam-hayes--phantom.png",
+    "Phantom - Liam Hayes",
+    22.45,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/damon-cross--rebirth.png",
+    "Rebirth - Damon Cross",
+    23.5,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/emilia-fox--resonance.png",
+    "Resonance - Emilia Fox",
+    19.5,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/oliver-reid--shadow-queen.png",
+    "Shadow Queen - Oliver Reid",
+    21.99,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/noah-black--silent-waves.png",
+    "Silent Waves - Noah Black",
+    23.55,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/naomi-woods--specter.png",
+    "Specter - Naomi Woods",
+    29.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/sophie-lark--starlight.png",
+    "Starlight - Sophie Lark",
+    32.55,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/lara-miles--stars-of-andromeda.png",
+    "Stars of Andromeda - Lara Miles",
+    29.99,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/chloe-woods--stolen-hearts.png",
+    "Stolen Hearts - Chloe Woods",
+    21.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/alex-draven--the-quantum-prison.png",
+    "The Quantum Prison - Alex Draven",
+    17.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/lila-gray--the-shadow.png",
+    "The Shadow - Lila Gray",
+    13.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/amelia-pierce--the-thief-of-time.png",
+    "The Thief of Time - Amelia Pierce",
+    18.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/oliver-reid--the-wild-hunt.png",
+    "The Wild Hunt - Oliver Reid",
+    10.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/edgar-hale--thorn-of-the-frost-king.png",
+    "Thorn of The Frost King - Edgar Hale",
+    29.45,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/marina-holt--tides.png",
+    "Tides - Marina Holt",
+    25.22,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/rebecca-cole--unseen-eyes.png",
+    "Unseen Eyes - Rebecca Cole",
+    27.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/lily-monroe--whispered-secrets.png",
+    "Whispered Secrets - Lily Monroe",
+    20.0,
+    "Lifestyle"
+  )
+);
+list.push(
+  new Book(
+    "img/books/lily-monroe--shattered-dreams.png",
+    "Shattered Dreams - Lily Monroe",
+    19.99,
+    "Lifestyle"
+  )
+);
+
+const parent = document.getElementById("all-books_books");
+const loadMoreBtn = document.getElementById("all-books__load-more");
+const BOOKS_PER_PAGE = 6;
+let currentPage = 0;
+
+function renderBooks() {
+  const start = currentPage * BOOKS_PER_PAGE;
+  const end = start + BOOKS_PER_PAGE;
+  const currentBooks = list.slice(start, end);
+
+  currentBooks.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("all-books__book");
+
+    const img = document.createElement("img");
+    img.src = book.image;
+    img.alt = book.name;
+
+    const name = document.createElement("h4");
+    name.textContent = book.name;
+
+    const price = document.createElement("p");
+    price.textContent = `$${book.price.toFixed(2)} USD`;
+
+    const button = document.createElement("a");
+    button.href = "#";
+    button.textContent = "Buy Now";
+
+    bookDiv.appendChild(img);
+    bookDiv.appendChild(name);
+    bookDiv.appendChild(price);
+    bookDiv.appendChild(button);
+
+    parent.appendChild(bookDiv);
+  });
+
+  currentPage++;
+
+  // Ховаємо кнопку, якщо всі книги вже показані
+  if (currentPage * BOOKS_PER_PAGE >= list.length) {
+    loadMoreBtn.style.display = "none";
+  }
+}
+
+// Показати перші 6 книг
+renderBooks();
+
+// Обробка натискання на "Load More"
+loadMoreBtn.addEventListener("click", () => {
+  renderBooks();
+});
+
+//noUiSlider
+const slider = document.getElementById("all-books__price-slider");
+const inputMin = document.querySelector(".all-books__price-input--min");
+const inputMax = document.querySelector(".all-books__price-input--max");
+
+noUiSlider.create(slider, {
+  start: [13, 32.5],
+  connect: true,
+  step: 0.5,
+  range: {
+    min: 13,
+    max: 32.5,
+  },
+});
+
+slider.noUiSlider.on("update", function (values, handle) {
+  const [min, max] = values;
+  inputMin.value = min;
+  inputMax.value = max;
+});
+
+inputMin.addEventListener("change", function () {
+  slider.noUiSlider.set([this.value, null]);
+});
+
+inputMax.addEventListener("change", function () {
+  slider.noUiSlider.set([null, this.value]);
+});
+
+document.getElementById("all-books__filters-button").addEventListener("click", () => {
+    const modal = document.getElementById("all-books__filters-modal");
+    const modalContent = document.querySelector(".all-books__modal-content");
+    const options = document.querySelector(".all-books__options");
+    modal.style.display = "block";
   
+    modalContent.innerHTML = '<button class="all-books__modal-close" id="close-filters-modal">✖</button>';
+  
+    const optionsClone = options.cloneNode(true);
+    optionsClone.style.display = "block";
+    modalContent.appendChild(optionsClone);
+  
+    const closeButton = modalContent.querySelector("#close-filters-modal");
+    if (closeButton) {
+      closeButton.addEventListener("click", closeModal);
+    } else {
+      console.warn("Close modal button not found");
+    }
+  });
+  
+  function closeModal() {
+    document.getElementById("all-books__filters-modal").style.display = "none";
+  }
+
+//End Max
