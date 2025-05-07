f043cf09814fa0253b4b1c8c869121344a29a3ef
Diana Shymanska <121499947+dianajnxv@users.noreply.github.com>
2025-05-07 20:14:01 +0000

    fixed styles

--- js/index.testimonials-readers.js
+++ js/index.testimonials-readers.js
@@ -3,40 +3,42 @@ const testimonials = [
     text: "“I’ve been using this site for over a year, and I love how easy it is to browse and buy eBooks. The selection is vast, and I always find something new to read. Highly recommended!”",
     name: "James M.",
     img: "img/testimonials/james.webp",
+    bgClass: "testimonials--james"
   },
   {
     text: "“The user experience here is fantastic. I love how easy it is to find exactly what I'm looking for, plus the recommendations have helped me discover some amazing new authors!”",
     name: "Sarah K.",
     img: "img/testimonials/sara.webp",
+    bgClass: "testimonials--sarah"
   },
   {
     text: "“I've been a book lover for years, and this site is now my go-to! The selection is impressive, especially for romance and thriller fans like me. I always find something fresh and exciting!”",
     name: "Kathleen J.",
     img: "img/testimonials/kathleen.webp",
+    bgClass: "testimonials--kathleen"
   },
   {
     text: "“So many eBooks for affordable prices! I appreciate that they feature both bestselling authors and new talent, making every visit a chance to find something cool.”",
     name: "Jadis E.",
     img: "img/testimonials/jadis.webp",
+    bgClass: "testimonials--jadis"
   },
 ];
 
 const carouselTrack = document.getElementById("carouselTrack");
 
-// Вставка відгуків у карусель
 testimonials.forEach((t) => {
   carouselTrack.innerHTML += createCard(t);
 });
 
-// Клонування слайдів для безкінечного ефекту
 testimonials.forEach((t) => {
-  carouselTrack.innerHTML += createCard(t); // Додати ще раз слайди в кінець для циклічності
+  carouselTrack.innerHTML += createCard(t); 
 });
 
 function createCard(t) {
   return `
         <div class="testimonials__card">
-          <div class="testimonials__inner">
+          <div class="testimonials__inner ${t.bgClass}">
             <img src="${t.img}" class="testimonials__img" alt="">
             <p>${t.text}</p>
             <div class="testimonials__name">— ${t.name}</div>
@@ -60,13 +62,11 @@ function updateCardWidths() {
   visibleCards = getVisibleCardsCount();
   cardWidthPercent = 100 / visibleCards;
 
-  // Встановлення без анімації перед зміною
   carouselTrack.style.transition = "none";
   carouselTrack.style.transform = `translateX(${
     -position * cardWidthPercent
   }%)`;
 
-  // Відновлення анімації через коротку затримку
   setTimeout(() => {
     carouselTrack.style.transition = "transform 0.5s ease-in-out";
   }, 0);
@@ -76,15 +76,14 @@ function moveCarousel(direction) {
   position += direction;
   const totalCards = testimonials.length;
 
-  // Циклічне переміщення
   if (position < 0) {
-    position = totalCards; // Крок назад до початку каруселі
+    position = totalCards; 
     updateCarouselPosition();
     return;
   }
 
   if (position >= totalCards) {
-    position = 0; // Крок вперед до першого елемента
+    position = 0; 
     updateCarouselPosition();
     return;
   }
@@ -98,13 +97,11 @@ function updateCarouselPosition() {
     -position * cardWidthPercent
   }%)`;
 
-  // Відновлення анімації
   setTimeout(() => {
     carouselTrack.style.transition = "transform 0.5s ease-in-out";
   }, 0);
 }
 
-// Ініціалізація
 updateCardWidths();
 window.addEventListener("resize", updateCardWidths);
 
